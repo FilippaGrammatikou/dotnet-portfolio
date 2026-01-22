acc8b1c5d2ac695061589ee8cbe243a6f5953db8
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-22 19:31:27 +0200

    Ch03: update comments

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch03_Variables.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch03_Variables.cs
@@ -4,8 +4,7 @@ public static class Ch03_Variables
 {
     public static void Run()
     {
-        // Bro Code chapter 3: Variables
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 3: Variables
+        
     }
 }
