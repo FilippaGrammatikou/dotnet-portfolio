0f5169568685be06ea719a4f1bd4e365456db596
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-24 19:53:45 +0200

    Add Ch10 Hypotenuse Calculator console app

---
+++ 02-mini-projects/Ch10-HypotenuseCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Mini Project — Ch10: Hypotenuse Calculator (Right Triangle)
+    -Computes hypotenuse length from legs a and b using:
+        c = √(a² + b²)
+    -Requirements:
+       a and b are numeric (double) and non-negative.
+
+    Example:
+      a = 3, b = 4  =>  c = 5 */
+
+namespace Ch10_HypotenuseCalculator;
+public static class Ch10_HypotenuseCalculator
+{
+    public static void Run()
+    {
+     
+
+        Console.ReadKey();
+    }
+}
