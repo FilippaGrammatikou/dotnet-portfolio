e87ee919d9a958cbd8b85e1d24424b96375283bb
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-25 18:47:58 +0200

    Ch12: add if/else decision examples (age + empty input checks)

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch12_IfStatements.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch12_IfStatements.cs
@@ -4,8 +4,34 @@ public static class Ch12_IfStatements
 {
     public static void Run()
     {
-        // Bro Code chapter 12: IfStatements
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 12: If Statements
+        // basic decision making form
+
+        /* Console.WriteLine("Please enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        
+        if(age >= 18 && age <= 119){
+            Console.WriteLine("You are now signed up!");
+        }
+        else if (age < 0){
+            Console.WriteLine("Please enter a valid age!");
+        }
+        else if (age > 120){
+            Console.WriteLine("Please enter a valid age!");
+        }
+        else{
+            Console.WriteLine("You must be 18+ to sign up to this service!");
+        } */
+
+        Console.WriteLine("Please enter your name: ");
+        String name = Console.ReadLine();
+
+        if (name == "") { // or if (name != "") console.writeline("hello, " + name)}
+            Console.WriteLine("You did not enter your name");
+        }
+        else{
+            Console.WriteLine("Hello, " + name);
+        }
+            Console.ReadKey();
     }
 }
