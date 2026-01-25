f7e4a6be9d819bf8adf08124b0f5e9e0e5a3d129
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-25 19:18:07 +0200

    Chapter 13 edits

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch13_SwitchStatements.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch13_SwitchStatements.cs
@@ -4,8 +4,10 @@ public static class Ch13_SwitchStatements
 {
     public static void Run()
     {
-        // Bro Code chapter 13: SwitchStatements
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 13: SwitchStatements
+
+
+        Console.WriteLine();
+        Console.ReadKey();
     }
 }
