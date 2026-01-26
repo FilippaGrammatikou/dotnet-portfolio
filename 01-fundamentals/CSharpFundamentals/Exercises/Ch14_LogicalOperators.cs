8853c21cfe50804f88a9a01ce5b9b8bf1455ed20
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-26 16:44:53 +0200

    Ch14: add logical operator temperature checks (&&, ||)

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch14_LogicalOperators.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch14_LogicalOperators.cs
@@ -4,8 +4,21 @@ public static class Ch14_LogicalOperators
 {
     public static void Run()
     {
-        // Bro Code chapter 14: LogicalOperators
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 14: LogicalOperators
+        // logical operators = can be used to check if more than one condition is true/false
+
+        // && (AND)
+        // || (OR)
+
+        Console.WriteLine("What's the temperature outside: (C)");
+        double temp = Convert.ToDouble(Console.ReadLine()); // converts the user input into a double since user input is always a string
+
+        if (temp >= 0 && temp <=25){ // with && both conditions must be true
+            Console.WriteLine("It's warm outside");
+        }
+        else if (temp <=-50 || temp>=50){ // with || only one condition needs to be true
+            Console.WriteLine("Do not go outside!");
+        }
+            Console.ReadKey();
     }
 }
