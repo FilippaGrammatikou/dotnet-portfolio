b7db1b184369a768991721c130767fddc663086b
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-26 13:51:03 +0200

    Ch13: add default case + normalize input (ToLower)

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch13_SwitchStatements.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch13_SwitchStatements.cs
@@ -5,9 +5,39 @@ public static class Ch13_SwitchStatements
     public static void Run()
     {
         // Chapter 13: SwitchStatements
+        // An efficient alternative to many else if statements
 
+        Console.WriteLine("What day is it today");
+        String day = (Console.ReadLine());
+        day = day.ToLower();
 
-        Console.WriteLine();
+        switch (day)
+        {
+            case "monday":
+                Console.WriteLine("It is Monday");
+                break;
+            case "tuesday":
+                Console.WriteLine("It is Tuesday");
+                break;
+            case "wednesday":
+                Console.WriteLine("It is Wednesday");
+                break;
+            case "thursday":
+                Console.WriteLine("It is Thursday");
+                break;
+            case "friday":
+                Console.WriteLine("It is Friday");
+                break;
+            case "saturday":
+                Console.WriteLine("It is Saturday");
+                break;
+            case "sunday":
+                Console.WriteLine("It is Sunday");
+                break;
+            default:
+                Console.WriteLine("Not a valid day. Type e.g. Monday.");
+                break;
+        }
         Console.ReadKey();
     }
 }
