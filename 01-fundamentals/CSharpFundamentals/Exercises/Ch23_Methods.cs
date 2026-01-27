9594c18c0af1b0811b26e2d2d36c84ef9da2286d
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-27 20:05:10 +0200

    ch23 additions

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch23_Methods.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch23_Methods.cs
@@ -8,17 +8,31 @@ public static class Ch23_Methods
         // performs a selection of codel, whenever it's called "invoked"
         // benefit = let's us reuse code w/o writing it multiple times
 
-        singHappyBirthday();
-        singHappyBirthday();
-        singHappyBirthday();
+        String name = "Random";
+        int age = 25;
+
+        singHappyBirthday(name, age); // pass down the name and age argument
+        //singHappyBirthday();
+        //singHappyBirthday();
 
         Console.ReadKey();
     }
-        static void singHappyBirthday(){
+        static void singHappyBirthday(String name, int age){ // pass down the name and age argument
             Console.WriteLine("Happy birthday to you!");
             Console.WriteLine("Happy birthday to you!");
-            Console.WriteLine("Happy birthday dear YOU");
+            Console.WriteLine("Happy birthday dear, " + name);
+            Console.WriteLine("You are " + age + " years old!");
             Console.WriteLine("Happy birthday to you!");
             Console.WriteLine();
         }
 }
+
+// static void singHappyBirthday(String bdayBoy, int yearsOld){ // can have different names
+/*        Console.WriteLine("Happy birthday to you!");
+           Console.WriteLine("Happy birthday to you!");
+           Console.WriteLine("Happy birthday dear, " + bdayBoy);
+           Console.WriteLine("You are " + yearsOld + " years old!");
+           Console.WriteLine("Happy birthday to you!");
+           Console.WriteLine();
+       }
+} */
