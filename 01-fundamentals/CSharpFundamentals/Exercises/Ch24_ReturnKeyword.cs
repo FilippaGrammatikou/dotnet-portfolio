1a7dd275284c39f647940c3edbedf313b7fc3f73
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-27 19:57:39 +0200

    ch24 comments

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch24_ReturnKeyword.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch24_ReturnKeyword.cs
@@ -4,8 +4,10 @@ public static class Ch24_ReturnKeyword
 {
     public static void Run()
     {
-        // Bro Code chapter 24: ReturnKeyword
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 24: ReturnKeyword
+        
+
+
+        Console.ReadKey();
     }
 }
