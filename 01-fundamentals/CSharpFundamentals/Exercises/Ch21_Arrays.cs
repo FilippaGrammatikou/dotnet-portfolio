1d1683d46ea1b2ff0c6fb15dc66257f120fb3ad7
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-27 19:28:10 +0200

    Ch21: Arrays

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch21_Arrays.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch21_Arrays.cs
@@ -4,8 +4,19 @@ public static class Ch21_Arrays
 {
     public static void Run()
     {
-        // Bro Code chapter 21: Arrays
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 21: Arrays
+        // async variable that can store multiple values. fixed sized
+        
+        String[] cars = {"BMW", "MUSTANG", "CORVETTE"};
+
+        cars[0] = "Tesla"; // updates the array
+        cars[1] = "Mustand";
+        cars[2] = "Corvette";
+
+        for (int i = 0; i < cars.Length; i++){
+            Console.WriteLine(cars[i]);
+        }
+
+        //String[] cars = new String[3]; declaring the array
     }
 }
