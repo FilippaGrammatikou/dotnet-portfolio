a189d4ebd1de7165ecb7d4c25fed44afa0914f32
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-27 20:20:01 +0200

    ch24 return usage

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch24_ReturnKeyword.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch24_ReturnKeyword.cs
@@ -1,13 +1,32 @@
-﻿namespace CSharpFundamentals.Exercises;
+﻿using System.Numerics;
+
+namespace CSharpFundamentals.Exercises;
 
 public static class Ch24_ReturnKeyword
 {
     public static void Run()
     {
         // Chapter 24: ReturnKeyword
-        
+        // returns data back to the place where a method is invoked
+        double x;
+        double y;
+        double result;
 
+        Console.WriteLine("Enter in number 1: ");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter in number 2: ");
+        y= Convert.ToDouble(Console.ReadLine());
+
+        result = Multiply(x, y);
+        Console.WriteLine(result);
 
         Console.ReadKey();
+        }
+
+    static double Multiply(double x, double y)
+    {
+        double z = x * y;
+        return z;
     }
 }
