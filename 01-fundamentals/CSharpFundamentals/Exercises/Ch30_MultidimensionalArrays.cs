4ca3dcb70b49255df73a80cf5fe6584dd833d821
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-28 15:21:14 +0200

    Ch30: Setting up and using Multidimensional Arrays

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch30_MultidimensionalArrays.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch30_MultidimensionalArrays.cs
@@ -4,8 +4,35 @@ public static class Ch30_MultidimensionalArrays
 {
     public static void Run()
     {
-        // Bro Code chapter 30: MultidimensionalArrays
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 30: MultidimensionalArrays
+
+        /* String[] ford = { "Mustang", "F-150", "Explorer" };
+        String[] chevy = { "Corvette", "Camaro", "Silverado" };
+        String[] toyota = { "Corolla", "Camry", "Rav4" }; */
+
+        String[,] parkingLot = {  { "Mustand", "F-150", "Explorer"},
+                                                { "Corvette", "Camaro", "Silverado" },
+                                                { "Corolla", "Camry", "Rav4"}
+                                             }; //once we complete this array, we don't need the ones from above anymore
+
+        parkingLot[0, 2] = "Fusion"; //we can change the values within the array by declaring the exact position in the multidimensional array
+        parkingLot[2,0] = "Tacoma";
+
+        /*foreach(String car in parkingLot)
+        {
+            Console.WriteLine(car);
+        }*/
+
+        //using nested for loops, we display the array like a grid/matrix
+        for(int i = 0; i < parkingLot.GetLength(0); i++) //getLength in the first loop always starts with 0
+        {
+            for(int j=0; j < parkingLot.GetLength(1); j++) // 1 so that this represents the second dimension
+            {
+                Console.WriteLine(parkingLot[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
     }
 }
