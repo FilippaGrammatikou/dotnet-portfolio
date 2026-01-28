4483374cc0026e88c42ad99fc954a2de73d59607
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-28 15:38:56 +0200

    Ch31: add classes demo (static methods + simple message class)

---
+++ 01-fundamentals/CSharpFundamentals/ClassChapter31.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals
+{
+    static class ClassChapter31
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program!");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for an action");
+        }
+
+        public static void Goodbye()
+        {
+            Console.WriteLine("Thank you for visiting this program!");
+        }
+    }
+}
--- 01-fundamentals/CSharpFundamentals/Exercises/Ch31_Classes.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch31_Classes.cs
@@ -4,8 +4,37 @@ public static class Ch31_Classes
 {
     public static void Run()
     {
-        // Bro Code chapter 31: Classes
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 31: Classes
+        // A bundle of related code we wish to organize together. Can be used as a blueprint to create objects (oop)
+        // Math.Round(3.14); // clicking on the class allows us to see all the math related methods withing it
+
+        // in order to call them, we either need some sort of object or we turn the class into static and the methods within as public static
+        ClassChapter31.Hello();
+        ClassChapter31.Waiting();
+        ClassChapter31.Goodbye();
+
+        Console.ReadKey();
     }
+
+    // To create a class we need to ensure that we are witting our code outside of other classes
+
+    /*  class Messages
+     {
+         void Hello()
+         {
+             Console.WriteLine("Hello! Welcome to the program!");
+         }
+
+         void Waiting()
+         {
+             Console.WriteLine("I am waiting for an action");
+         }
+
+         void Goodbye()
+         {
+             Console.WriteLine("Thank you for visiting this program!");
+         }
+     } */
+    // Or we can create a class via the solution explorer.
+
 }
