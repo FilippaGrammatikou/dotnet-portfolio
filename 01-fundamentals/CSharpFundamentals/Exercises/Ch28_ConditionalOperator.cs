59ac0a14a1f997a79da9a26576af83179684359b
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-28 14:43:19 +0200

    Chapter 28: Conditional Operator Shortcuts

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch28_ConditionalOperator.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch28_ConditionalOperator.cs
@@ -4,8 +4,23 @@ public static class Ch28_ConditionalOperator
 {
     public static void Run()
     {
-        // Bro Code chapter 28: ConditionalOperator
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 28: ConditionalOperator
+        // used in conditional assignment if a condition is true/false
+        // (condition) ? x : y
+
+        double temp = 20;
+        // String message;
+
+        /* if (temp >= 15) {
+            message = "It's warm outside";
+        }
+        else {
+            message = "It's cold outside";
+        } */
+
+        // message = (temp >= 15) ? "It's warm outside!" : "It's cold outside!"; // mesage not needed if we do the following:
+        Console.WriteLine((temp >= 15) ? "It's warm outside!" : "It's cold outside!");
+
+        Console.ReadKey();
     }
 }
