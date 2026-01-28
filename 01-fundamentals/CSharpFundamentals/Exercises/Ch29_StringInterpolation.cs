f6269b19d98a185eb434cdaa9419bcceed2b7c5d
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-28 14:59:28 +0200

    Ch29_String Interpolation Wiser Coding Practice

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch29_StringInterpolation.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch29_StringInterpolation.cs
@@ -5,7 +5,24 @@ public static class Ch29_StringInterpolation
     public static void Run()
     {
         // Bro Code chapter 29: StringInterpolation
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // allows us to insert variables into a string literal
+        // precede a string literal with $
+        // {} are placeholders
+
+        String firstName = "Random";
+        String lastName = "Person";
+        int age = 21;
+
+        /* Console.WriteLine("Hello, " + firstName + " " + lastName ".");
+        Console.WriteLine("You are " + age + " years old."); */
+
+        // BETTER practice:
+
+        Console.WriteLine($"Hello, {firstName} {lastName}.");
+        Console.WriteLine($"You are {age} years old.");
+        // Console.WriteLine($"You are {age, 10} years old."); //creates a gap of 10 spaces by the age including the digits of the int
+        
+
+        Console.ReadKey();
     }
 }
