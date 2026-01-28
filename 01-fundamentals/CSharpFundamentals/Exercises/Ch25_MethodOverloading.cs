b8b36e608ff8e2c31d192e9d0784bfa9a150bddd
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-28 13:13:55 +0200

    Ch25 adittions to Multiply

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch25_MethodOverloading.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch25_MethodOverloading.cs
@@ -6,18 +6,24 @@ public static class Ch25_MethodOverloading
     {
         // Chapter 25: MethodOverloading
         // methods share same name, but different parameters
-        //name + parameters = signature
+        // name + parameters = signature
         // methods must have a unique signature
 
         double total;
 
-        total = Multiply(2, 3);
+        total = Multiply(2, 3, 4);
 
         Console.WriteLine(total);
         Console.ReadLine();
     }
 
-    static double Multiply (double a, double b){
-        return a* b;
-        }
+    static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    static double Multiply(double a, double b, double c)
+    {
+        return a * b * c;
+    }
 }
