50665cf7ddf2c4211d999560c9bc96784a2300b4
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-30 19:18:59 +0200

    Ch35: demonstrate constructor overloading with optional ingredients

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch35_OverloadedConstructors.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch35_OverloadedConstructors.cs
@@ -4,8 +4,45 @@ public static class Ch35_OverloadedConstructors
 {
     public static void Run()
     {
-        // Bro Code chapter 35: OverloadedConstructors
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 35: OverloadedConstructors
+        // technique to create multiple constructors, with a different set of parameters
+        // name + parameters = signature
+
+        Pizza pizza = new Pizza("stuffed crust", "red sauce", "mozzarella"/*, "pepperoni"*/);
+        // if we wish for some of the ingredient categories to be missing eg toppings, we need to use overloaded constructors
+
+        Console.ReadKey();
+    }
+
+    class Pizza
+    {
+        String bread;
+        String sauce;
+        String cheese;
+        String topping;
+
+        public Pizza(String bread)
+        {
+            this.bread = bread;
+        }
+        public Pizza(String bread, String sauce)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+        }
+        public Pizza(String bread, String sauce, String cheese)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;
+        }
+        public Pizza(String bread, String sauce, String cheese, String topping)
+        {
+            this.bread = bread;
+            this.sauce = sauce; 
+            this.cheese = cheese;
+            this.topping = topping;
+        }
+
     }
 }
