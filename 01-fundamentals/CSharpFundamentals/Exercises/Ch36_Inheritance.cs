b16ea3be69b3bd541a7b8124d5730c6ffa0bcfaf
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-30 20:37:31 +0200

    Initiation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch36_Inheritance.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch36_Inheritance.cs
@@ -4,8 +4,36 @@ public static class Ch36_Inheritance
 {
     public static void Run()
     {
-        // Bro Code chapter 36: Inheritance
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 36: Inheritance
+        // One or more CHILD classes receiving fields, methods, etc. from a common parent
+
+
+        Console.ReadKey(); 
     }
+
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+    }
+
+    class Bike : Vehicle
+    {
+        public int wheels = 2;
+    }
+
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+    }
+
 }
