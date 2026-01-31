c295ef827d90be330ef17401e1c6b11a193e9cd9
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-31 14:51:34 +0200

    Ch37: demonstrate abstract class prevents instantiation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch37_AbstractClasses.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch37_AbstractClasses.cs
@@ -10,16 +10,18 @@ public static class Ch37_AbstractClasses
         Car car = new Car();
         Bike bike = new Bike();
         Boat boat = new Boat();
-        Vehicle vehicle = new Vehicle();
-
+        // Vehicle vehicle = new Vehicle(); 
+        // vehicle will not have wheels and maxspeed since it's a parent, which is wrong use of code
+        // incomplete implemantation, should be avoided from usage
         Console.ReadKey();  
     }
 
-    class Vehicle
-    {
-        public int speed = 0;
+    //parent classes do't inherit methods and fields from the children classes
+    abstract class Vehicle // abstract prevents instantiating incomplete implementation of the parent class 
+    {                                 // (abstract can be applied to properties and methods as well)
+        public int speed = 0; // field
 
-        public void go()
+        public void go()  // method
         {
             Console.WriteLine("This vehicle is moving");
         }
