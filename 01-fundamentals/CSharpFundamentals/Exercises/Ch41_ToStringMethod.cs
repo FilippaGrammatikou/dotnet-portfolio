fa32053c7b79cab7225138769b6531350ca0f062
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-31 18:28:39 +0200

    Ch41: show default ToString vs overridden output

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch41_ToStringMethod.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch41_ToStringMethod.cs
@@ -4,8 +4,38 @@ public static class Ch41_ToStringMethod
 {
     public static void Run()
     {
-        // Bro Code chapter 41: ToStringMethod
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 41: ToStringMethod (built in)
+        // converts an object to its string representation so that it is suitable for display
+
+        Car car = new Car("Chevy", "Corvette", 2022, "blue");
+
+        Console.WriteLine(car.ToString()); // displays the namespace followed by the type of object we are working with
+        // in this case: CSharpFundamentals.Exercises.Ch41_ToStringMethod+Car
+
+        // or: Console.WriteLine(car);
+
+        Console.ReadKey();
+    }
+    class Car
+    {
+        String make;
+        String model;
+        int year;
+        String colour;
+
+        public Car(String make, String model, int year, String colour)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.colour = colour;
+        }
+        public override string ToString() // be sure to return a string
+        {
+            /* String mesage = "This is a " + make + " " + model; // can either do this
+            return base.ToString(); */
+
+            return "This is a " + make + " " + model+"."; // or this
+        }
     }
 }
