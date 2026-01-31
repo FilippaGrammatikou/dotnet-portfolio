446e8f07314253f4877eb2578c8142ca62de7a9b
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-01-31 14:30:31 +0200

    initiation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch37_AbstractClasses.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch37_AbstractClasses.cs
@@ -4,8 +4,39 @@ public static class Ch37_AbstractClasses
 {
     public static void Run()
     {
-        // Bro Code chapter 37: AbstractClasses
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 37: AbstractClasses
+        // modifier that indicates missing components or incomplete implementation
+
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
+        Vehicle vehicle = new Vehicle();
+
+        Console.ReadKey();  
+    }
+
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving");
+        }
+    }
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+        int maxSpeed = 500;
+    }
+    class Bike : Vehicle
+    {
+        public int wheels = 2;
+        int maxSpeed = 50;
+    }
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+        int maxSpeed = 200;
     }
 }
