2bdc7d828c6d5ed9bc27297cd1d7f917d9c20f98
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-01 20:13:01 +0200

    Initiation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch44_Lists.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch44_Lists.cs
@@ -4,8 +4,24 @@ public static class Ch44_Lists
 {
     public static void Run()
     {
-        // Bro Code chapter 44: Lists
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 44: Lists
+        // data structure that represents a list of objects that can be accessed by index
+        // similar to arrays, but can DYNAMICALLY increase/decrease in size
+        // using System.Collections.Generic; 
+
+        String[] food = new string[3];
+
+        food[0] = "pizza";
+        food[1] = "hamburger";
+        food[2] = "steak";
+        food[3] = "fries";
+
+        foreach (String item in food)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadKey();
     }
+
 }
