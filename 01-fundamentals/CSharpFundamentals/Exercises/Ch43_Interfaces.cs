327bdc1ed46f5eb57bd5885a73f8941acc20f240
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-01 15:26:31 +0200

    Ch43: document interfaces as contracts + multi-interface implementation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch43_Interfaces.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch43_Interfaces.cs
@@ -11,6 +11,54 @@ public static class Ch43_Interfaces
         // Benefit = security + multiple inheritance + "plug-and-play"
 
 
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.Flee();
+        hawk.Hunt();
+        fish.Flee();
+        fish.Hunt();
+
         Console.ReadKey();
     }
+
+    interface IPrey
+    {
+        void Flee();
+    }
+
+    interface IPredator
+    {
+        void Hunt();
+    }
+
+    class Rabbit : IPrey
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The rabbit runs away.");
+        }
+    }
+
+    class Hawk : IPredator
+    {
+        public void Hunt()
+        {
+            Console.WriteLine("The hawk searches for food.");
+        }
+    }
+
+    class Fish : IPrey, IPredator // with interfaces a class can inherit more than one methods unlike with standard inheritance
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The fish swims away!");
+        }
+        public void Hunt()
+        {
+            Console.WriteLine("The fish is searching for smaller fish!");
+        }
+    }
+
 }
