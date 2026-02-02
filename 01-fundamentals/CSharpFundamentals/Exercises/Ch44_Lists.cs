8917c102dc6d2a03bdb0bc7657f1faa3daa5444e
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-02 18:16:37 +0200

    Ch44: add List<string> demo + common operations (Add/Remove/Insert/etc.)

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch44_Lists.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch44_Lists.cs
@@ -1,4 +1,7 @@
-﻿namespace CSharpFundamentals.Exercises;
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpFundamentals.Exercises;
 
 public static class Ch44_Lists
 {
@@ -9,13 +12,33 @@ public static class Ch44_Lists
         // similar to arrays, but can DYNAMICALLY increase/decrease in size
         // using System.Collections.Generic; 
 
-        String[] food = new string[3];
+        /* String[] food = new string[3];
 
         food[0] = "pizza";
         food[1] = "hamburger";
         food[2] = "steak";
-        food[3] = "fries";
+        food[3] = "fries"; */
 
+        // Instead, use lists
+
+        List<string> food = new List<string>();
+        // food.Add("fries");
+        food.Add("pizza");
+        food.Add("hamburger");
+        food.Add("steak");
+        food.Add("fries");
+
+        // food.Remove("fries");
+        // food.Insert(0, "sushi");
+        // Console.WriteLine(food.Count);
+        // Console.WriteLine(food.IndexOf("pizza"));
+        // Console.WriteLine(food.LastIndexOf("fries"));
+        // Console.WriteLine(food.Contains("pizza"));
+        // food.Sort();
+        // food.Reverse();
+        // food.Clear();
+        // String[] foodArray = food.ToArray();
+        
         foreach (String item in food)
         {
             Console.WriteLine(item);
