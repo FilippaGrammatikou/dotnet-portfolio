03fcfc3b8849dcc761f6a5501b0ff27c0949d35d
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-03 16:55:18 +0200

    Ch45: demonstrate List<T> with custom Player class and foreach printing

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch45_ListOfObjects.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch45_ListOfObjects.cs
@@ -8,20 +8,26 @@ public static class Ch45_ListOfObjects
     public static void Run()
     {
         // Chapter 45: ListOfObjects
+        // list of objects from a custom class
 
-        List<Player> players = new List<Player>();
-        /*Player player1 = new Player("Chad");
+        List<Player> players = new List<Player>(); // new list <data type, in this case: our class>
+
+        /*Player player1 = new Player("Chad"); //instantiating player objects
         Player player2 = new Player("Dean");
-        Player player3 = new Player("Larry"); */
+        Player player3 = new Player("Larry"); 
 
-        players.Add(new Player("Chad"));
+        players.Add(player1); // adds objects in the list
+        players.Add(player2);
+        players.Add(player3);*/
+
+        players.Add(new Player("Chad")); //instantiating the objects anonymously
         players.Add(new Player("Dean"));
         players.Add(new Player("Larry"));
 
         foreach (Player player in players)
         {
-            Console.WriteLine(player.username);
-        }
+            Console.WriteLine(player.username); //needs to add username, else displays class datatype
+        } // or  we can override the ToString method
 
         Console.ReadKey();
     }
