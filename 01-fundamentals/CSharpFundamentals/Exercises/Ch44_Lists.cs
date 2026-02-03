2ca8096ec3ee378ae37593fafb35941e205e6443
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-03 16:44:49 +0200

    Ch44: document arrays vs lists + demo Add/Remove/Insert/Sort/ToArray

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch44_Lists.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch44_Lists.cs
@@ -12,33 +12,34 @@ public static class Ch44_Lists
         // similar to arrays, but can DYNAMICALLY increase/decrease in size
         // using System.Collections.Generic; 
 
-        /* String[] food = new string[3];
+        /* String[] food = new string[3]; //array
 
-        food[0] = "pizza";
+        food[0] = "pizza"; //adding elements in array
         food[1] = "hamburger";
         food[2] = "steak";
-        food[3] = "fries"; */
+        food[3] = "fries"; */ // if we add an extra element, we run into an unhandled exception :outside the bounds of the array
 
         // Instead, use lists
 
-        List<string> food = new List<string>();
+        List<string> food = new List<string>(); //list, type of object we want to store, name of list
+
         // food.Add("fries");
-        food.Add("pizza");
+        food.Add("pizza"); //adds items
         food.Add("hamburger");
         food.Add("steak");
         food.Add("fries");
 
-        // food.Remove("fries");
-        // food.Insert(0, "sushi");
-        // Console.WriteLine(food.Count);
-        // Console.WriteLine(food.IndexOf("pizza"));
-        // Console.WriteLine(food.LastIndexOf("fries"));
-        // Console.WriteLine(food.Contains("pizza"));
-        // food.Sort();
-        // food.Reverse();
-        // food.Clear();
-        // String[] foodArray = food.ToArray(); // declaring a new array
-        
+        // food.Remove("fries"); //removes item from list
+        // food.Insert(0, "sushi"); // insert an object at a given element, we need an index and an item, DOESN'T replace existing item in list
+        // Console.WriteLine(food.Count); // displays list size
+        // Console.WriteLine(food.IndexOf("pizza")); //displays positon of an item in the list
+        // Console.WriteLine(food.LastIndexOf("fries")); //displays numerical positon of the last item in the list
+        // Console.WriteLine(food.Contains("pizza"));  //returns a boolean
+        // food.Sort(); //lists the list alphabetically
+        // food.Reverse(); //lists the list alphabetically reversed
+        // food.Clear(); //clears the list
+        // String[] foodArray = food.ToArray(); // converts the list into an array declaring a new array
+
         foreach (String item in food)
         {
             Console.WriteLine(item);
