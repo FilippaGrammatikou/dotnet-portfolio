180425fcf50a6d06efdae19e141ad89125a4e2de
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-03 19:17:40 +0200

    initiation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch49_Generics.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch49_Generics.cs
@@ -4,8 +4,47 @@ public static class Ch49_Generics
 {
     public static void Run()
     {
-        // Bro Code chapter 49: Generics
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 49: Generics
+        // "not specified to a particular data type"
+        // add <T> to: classes, method, fields,etc.
+        // allows for code reusability for different data types
+
+        int[] intArray = { 1, 2, 3 };
+        double[] doubleArray = { 1.0, 2.0, 3.0 };
+        String[] stringArray = { "1", "2", "3" };
+        
+        displayElements(intArray);
+        displayElements(doubleArray);
+        displayElements(stringArray);
+
+        Console.ReadKey();
+    }
+    /*public static void displayElements(int[] array)
+    {
+        foreach (int item in array)
+        {
+            Console.WriteLine(item + " ");
+        }
+    }
+    public static void displayElements(double[] array)
+    {
+        foreach(double item in array)
+        {
+            Console.WriteLine(item + " ");
+        }
+    }
+    public static void displayElements(String[] array)
+    {
+        foreach (String item in array)
+        {
+            Console.WriteLine(item + " ");
+        }
+    }*/
+    public static void displayElements<Thing>(Thing[] array)
+    {
+        foreach (Thing item in array)
+        {
+            Console.WriteLine(item + " ");
+        }
     }
 }
