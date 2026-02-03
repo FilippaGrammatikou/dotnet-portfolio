8dd4da62a82e959ba54a9241a3a1501cdfd61c9f
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-03 17:46:47 +0200

    Ch46: demonstrate private field + public property with get/set and clamping logic

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch46_GettersAndSetters.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch46_GettersAndSetters.cs
@@ -11,23 +11,40 @@ public static class Ch46_GettersAndSetters
 
         // properties = combine aspects of both fields and methods (share name with a field)
         // get accessor = used to return the property value
-        // get accessor = used to assign a new value
+        // set accessor = used to assign a new value
         // value keyword = defines the value being assigned by the set (parameter)
 
-        Car car = new Car(400);
-        car.speed = 100000000;
+        Car car = new Car(400); //because public Car(int speed), therefore new Car(400 -> speed field)
+        car.Speed = 1000000000; //accessing the property instead of the field
 
-        Console.WriteLine(car.speed);
+        Console.WriteLine(car.Speed); //accessing the property instead of the field if speed is written with a capital S
 
         Console.ReadKey();
     }
 
     class Car
     {
-        public int speed;
+        private int speed; //field but if we turned it to private, we would lose access
         public Car(int speed)
         {
             speed = speed;
+        }
+
+        public int Speed //property
+        {
+            get { return speed; }  //read whatever values are within the field
+            set // for this to be writable, we need set // writable
+            {
+                if (value > 500)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+                    speed = value; // value is like a parameter. when assigning a value to speed, that value is =value;
+            }
         }
     }
 }
