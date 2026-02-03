0da200dbb49820291af5a70d224f040f8c57ebb1
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-03 15:54:32 +0200

    initiation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch46_GettersAndSetters.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch46_GettersAndSetters.cs
@@ -1,11 +1,33 @@
-﻿namespace CSharpFundamentals.Exercises;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSharpFundamentals.Exercises;
 
 public static class Ch46_GettersAndSetters
 {
     public static void Run()
     {
-        // Bro Code chapter 46: GettersAndSetters
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 46: GettersAndSetters
+        // add security to fields by encapsulation. They're accessors found within properties
+
+        // properties = combine aspects of both fields and methods (share name with a field)
+        // get accessor = used to return the property value
+        // get accessor = used to assign a new value
+        // value keyword = defines the value being assigned by the set (parameter)
+
+        Car car = new Car(400);
+        car.speed = 100000000;
+
+        Console.WriteLine(car.speed);
+
+        Console.ReadKey();
+    }
+
+    class Car
+    {
+        public int speed;
+        public Car(int speed)
+        {
+            speed = speed;
+        }
     }
 }
