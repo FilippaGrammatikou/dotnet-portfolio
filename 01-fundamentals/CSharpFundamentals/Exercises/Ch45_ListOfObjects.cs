4cd98ab55c4e8f3f45983fa00e48603e68c265ba
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-03 15:41:16 +0200

    initiation

--- 01-fundamentals/CSharpFundamentals/Exercises/Ch45_ListOfObjects.cs
+++ 01-fundamentals/CSharpFundamentals/Exercises/Ch45_ListOfObjects.cs
@@ -1,11 +1,42 @@
-﻿namespace CSharpFundamentals.Exercises;
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpFundamentals.Exercises;
 
 public static class Ch45_ListOfObjects
 {
     public static void Run()
     {
-        // Bro Code chapter 45: ListOfObjects
-        // 1) Copy the tutorial examples here.
-        // 2) Add ONE mutation (your own variation) to prove understanding.
+        // Chapter 45: ListOfObjects
+
+        List<Player> players = new List<Player>();
+        Player player1 = new Player("Chad");
+        Player player2 = new Player("Dean");
+        Player player3 = new Player("Larry");
+
+        players.Add(player1);
+        players.Add(player2);
+        players.Add(player3);
+
+        foreach (Player player in players)
+        {
+            Console.WriteLine(player.username);
+        }
+
+        Console.ReadKey();
+    }
+
+    class Player
+    {
+        public string username;
+
+        public Player(String username)
+        {
+            this.username = username;
+        }
+        public override string ToString()
+        {
+            return username;
+        }
     }
 }
