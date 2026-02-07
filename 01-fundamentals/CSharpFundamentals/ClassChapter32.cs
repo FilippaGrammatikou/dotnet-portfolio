80c4da9ccb21cedfa6a5471529addb92d103c11c
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-07 14:45:34 +0200

    Human turns into an instance class

--- 01-fundamentals/CSharpFundamentals/ClassChapter32.cs
+++ 01-fundamentals/CSharpFundamentals/ClassChapter32.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CSharpFundamentals
 {
     class Human
     {
-        public String name; //not safe to make these public but just for learning porpuses
+        public string name; //not safe to make these public but just for learning porpuses
         public int age;
 
         public void Eat()
         {
             Console.WriteLine(name + " is eating.");
         }
+
         public void Sleeping()
         {
             Console.WriteLine(name + " is sleeping.");
         }
-
     }
 }
+
+
