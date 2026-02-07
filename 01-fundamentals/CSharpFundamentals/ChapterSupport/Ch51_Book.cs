8e91d9cae0581f484e0a85d0582c10ea65e08723
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-07 16:14:40 +0200

    Ch51: Notes on theory and Book class creation

---
+++ 01-fundamentals/CSharpFundamentals/ChapterSupport/Ch51_Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals.ChapterSupport
+{
+    public class Ch51_Book
+    {
+        public int Id { get; set;  }
+        public String Name { get; set; }
+        public double Price { get; set; }
+        public string Author { get; set; }
+        public Ch51_Book(int id, string name, double price, string author)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Author = author;
+        }
+    }
+}
--- 01-fundamentals/CSharpFundamentals/LINQ/Ch51_SelectOperator.cs
+++ 01-fundamentals/CSharpFundamentals/LINQ/Ch51_SelectOperator.cs
@@ -8,6 +8,30 @@ public static class Ch51_SelectOperator
 {
     public static void Run()
     {
+        // Chapter 51: NestedLoops
+        // Language Integrated Query || 2 syntaxes: Method Based & Query Based
+        // LINQ applications: linq to Objects(collections, files,Strings,etc)
+        //                                 linq to entities(entity framework)
+        //                                 ling to xml(xml docs)
+        //                                 linq to datasets(ado.net data sets)
+        //-------------------------------//
+
+        // Method based: var output = books.Where(condition); ->lead to lambda expressions
+        // Query based: var output = from b in books where condition
+        //                                                                       orderby property
+        //                                                                       select property; (query syntax MUST end with select or group clause)
+        // it is allowed to mix the syntaxes
+        //-------------------------------//
+
+        // SELECT OPERATOR
+        // used for selecting any particular property or object. Also allows to define a new type with selected property
+        // var authors = books.Select(b => b.Author);  OR var authors = from b in books select b.Author;
+        // defining a new type using Select
+        // var data = books.Select(b=>new{
+        //                  BookName = b.Name,
+        //                  Cost = b.Price});
+        //-------------------------------//
+
 
 
 
