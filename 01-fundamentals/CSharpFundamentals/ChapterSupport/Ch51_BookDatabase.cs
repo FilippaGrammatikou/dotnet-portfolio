72569f43cb0eb74415ac07a87aca144feccd6647
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-07 16:22:55 +0200

    Ch51_BookDatabase Class Creation and Relocation

---
+++ 01-fundamentals/CSharpFundamentals/ChapterSupport/Ch51_BookDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals.ChapterSupport
+{
+    public class Ch51_BookDatabase
+    {
+        public static IEnumerable<Ch51_Book> GetBooksData()
+        {
+            return new List<Ch51_Book>
+            {
+                new Ch51_Book(121, "Harry Potter and the Sorcere's Stone", 800, "J.K. Rowling"),
+                new Ch51_Book(186, "Tell me your Dreams", 600, "Sidney Sheldon"),
+                new Ch51_Book(112, "The Monk who sold his Ferrari", 350, "Robin Sharma"),
+                new Ch51_Book(145, "Half Girlfriend", 180, "Chetan Bhagat"),
+                new Ch51_Book(167, "Harry Potter and the Half Blood Prince", 800, "J.K. Rowling"),
+                new Ch51_Book (133, "Who will Cry When you Die", 250, "Robin Sharma"),
+                new Ch51_Book(177, "The 7 Habits of Highly Effective People", 1100,"Stephen Covey")
+            };
+        }
+    }
+}
