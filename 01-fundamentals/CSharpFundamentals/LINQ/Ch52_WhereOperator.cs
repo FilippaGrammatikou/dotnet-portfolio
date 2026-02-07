dbc5d893345244381990cb1fae9e9e0835b10a51
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-07 18:30:22 +0200

    Ch52 Where examples (method + query syntax) using BookDatabase

--- 01-fundamentals/CSharpFundamentals/LINQ/Ch52_WhereOperator.cs
+++ 01-fundamentals/CSharpFundamentals/LINQ/Ch52_WhereOperator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using CSharpFundamentals.ChapterSupport;
 
 
 namespace CSharpFundamentals.LINQ;
@@ -11,7 +12,32 @@ public static class Ch52_WhereOperator
     public static void Run()
     {
         // Chapter 51: WhereOperator
+        // Used for filtering data from a set of data
+        // eg:  var cheapBooks = books.Where(b=>b.Price<100); or var cheapBooks = from b in books where b.Price <100 select b;
 
 
-    }
+        var books = Ch51_BookDatabase.GetBooksData();  // imports book data
+
+        var data = books.Where(b => b.Price < 500); //METHOD based
+        // var data = from b in books  where b.price<500 select b; //QUERY based
+
+        foreach(var item in data)
+        {
+            Console.WriteLine($"{item.Name} - {item.Author} - ({item.Price})");
+        }
+
+
+        //if we want on;y the titles of the books that are under 500, then all we need to do is this:
+        /*var data = books.Where(b => b.Price < 500).Select(b=>b.Name);  // METHOD based
+        // var data = from b in books  where b.price<500 select b.Name;     //QUERY based
+         
+        foreach (var item in data)
+        {
+            Console.WriteLine($"{item.Name} - {item.Author}");
+        }*/
+
+
+
+        Console.ReadKey();
+        }
 }
