b1b671bef11eb1145421a3223672a0ce80abcf83
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-09 19:05:57 +0200

    Ch56: cheat sheet for common LINQ operators

--- 01-fundamentals/CSharpFundamentals/LINQ/Ch56_UsefulOperator.cs
+++ 01-fundamentals/CSharpFundamentals/LINQ/Ch56_UsefulOperator.cs
@@ -14,16 +14,14 @@ public static class Ch56_UsefulOperator
         // data.Single() ==> Looks for ONE item matching the condition. If not found or found more, throws exception
         // data.SingleOrDefault() ==> Same as previous. No exception. If not found, returns default value
         // data.First() ==> Looks for FIRST item matching the condition. If not found, throws exception
-        // data.FirstOrDefault() ==>
-        // data.Last() ==>
-        // data.LastOrDefault() ==>
-        // data.Min() ==>
-        // data.Max() ==>
-        // data.Average() ==>
-        // data.Sum() ==>
-        // data.Count() ==>
-
-
+        // data.FirstOrDefault() ==> Same as First(). No exception. If not found, returns default value
+        // data.Last() ==>  Looks for LAST item matching the condition. If not found, throws exception
+        // data.LastOrDefault() ==> Same as Last(). No exception. If not found, returns default value
+        // data.Min() ==> Gets the min from a list of numeric elements, or based on the property specified.
+        // data.Max() ==> Gets the max from a list of numeric elements, or based on the property specified.
+        // data.Average() ==> Gets the average from a list of numeric elements, or based on the property specified.
+        // data.Sum() ==> Gets the sum from a list of numeric elements, or based on the property specified.
+        // data.Count() ==> Gets the Count from a list of numeric elements, or based on the property specified.
     }
 }
 
