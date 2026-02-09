a5d29625acbd6119e270a8287f6416d9b0809ddc
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-09 19:00:31 +0200

    Initiation

--- 01-fundamentals/CSharpFundamentals/LINQ/Ch56_UsefulOperator.cs
+++ 01-fundamentals/CSharpFundamentals/LINQ/Ch56_UsefulOperator.cs
@@ -8,7 +8,20 @@ public static class Ch56_UsefulOperator
     {
     public static void Run()
     {
+        // Chapter 56: UsefulOperators
+        // Here is a following list of other useful LINQ operators and their use
 
+        // data.Single() ==> Looks for ONE item matching the condition. If not found or found more, throws exception
+        // data.SingleOrDefault() ==> Same as previous. No exception. If not found, returns default value
+        // data.First() ==> Looks for FIRST item matching the condition. If not found, throws exception
+        // data.FirstOrDefault() ==>
+        // data.Last() ==>
+        // data.LastOrDefault() ==>
+        // data.Min() ==>
+        // data.Max() ==>
+        // data.Average() ==>
+        // data.Sum() ==>
+        // data.Count() ==>
 
 
     }
