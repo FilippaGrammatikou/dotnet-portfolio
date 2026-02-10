6f640a1a81f787300425e14a74ec5a128dd472c1
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-10 18:31:25 +0200

    Initiation

--- 01-fundamentals/CSharpFundamentals/LINQ/Ch57_InnerJoin.cs
+++ 01-fundamentals/CSharpFundamentals/LINQ/Ch57_InnerJoin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CSharpFundamentals.LINQ;
 
@@ -8,6 +9,18 @@ public static class Ch57_InnerJoin
     {
     public static void Run()
     {
+        // Chapter 57: InnerJoin
+        // Joins are used for combining two sequences or collections and produce a result
+        // Similar to joins in SQL
+
+        // Most used Joins in LINQ:
+        // - Inner Join
+        // - Group Join
+        // - Left Outer Join
+
+        // Not so used Joins:
+        // - Non Equi Join
+        // - Cross Join
 
 
 
