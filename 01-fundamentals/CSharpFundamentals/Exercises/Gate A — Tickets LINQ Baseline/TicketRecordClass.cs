7c8799dedf047dd4b6f3b74d4bf677ab4b67182d
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-12 19:32:37 +0200

    Initiation for Ticket Project

---
+++ 01-fundamentals/CSharpFundamentals/Exercises/Gate A — Tickets LINQ Baseline/TicketRecordClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals.Exercises.Gate_A___Tickets_LINQ_Baseline
+{
+    internal class TicketRecordClass
+    {
+    }
+}
