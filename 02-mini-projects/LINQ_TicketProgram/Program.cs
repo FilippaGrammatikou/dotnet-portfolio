e64a50723be6c8c73a6a31d2ba1d14763d564365
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-15 15:42:00 +0200

    Initiation

--- 02-mini-projects/LINQ_TicketProgram/Program.cs
+++ 02-mini-projects/LINQ_TicketProgram/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LINQ_TicketProgram;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var tickets = Ticket_DB.GetTicketData();
+
+    }
+}
