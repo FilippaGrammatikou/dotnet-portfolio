825ca79274e177aafc585b9ec8733efc45d09324
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-16 17:14:01 +0200

    Task B: Sort by date

--- 02-mini-projects/LINQ_TicketProgram/Program.cs
+++ 02-mini-projects/LINQ_TicketProgram/Program.cs
@@ -12,6 +12,7 @@ public static class Program
         var tickets = Ticket_DB.GetTicketData();
 
         var openTickets = tickets.Where(t => t.Status == "Open").ToList();
+        var latestFirst = tickets.OrderByDescending(t => t.dOC);
 
         Console.WriteLine("The current Open tickets are: ");
         Console.WriteLine($"COUNT: {openTickets.Count}");
@@ -21,6 +22,19 @@ public static class Program
         {
             Console.WriteLine($"{ticket.Id}: {ticket.Name}");
         }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        
+        //New print result
+        Console.WriteLine("The top 3 latest tickets are: ");
+        Console.WriteLine()
+            ;
+        foreach(var ticket in latestFirst.Take(3)) // only picks 3 results
+        {
+            Console.WriteLine($"{ticket.Id}: {ticket.dOC} {ticket.Status} - {ticket.Name}");
+        }
+
 
         Console.ReadKey();
     }
