585bf4d38844191693f8187342f8eab4e57d9c4e
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-19 19:33:17 +0200

    Update seeded tickets

--- 02-mini-projects/LINQ_TicketProgram/Ticket_DB.cs
+++ 02-mini-projects/LINQ_TicketProgram/Ticket_DB.cs
@@ -12,14 +12,14 @@ namespace LINQ_TicketProgram
         {
             return new List<Ticket>
             {
-                new Ticket(1, "Login fails on invalid password","Open", DateTime.Today.AddDays(-1)),
-                new Ticket(2, "Add pagination to tickets endpoint","Open", DateTime.Today.AddDays(-4)),
-                new Ticket(3, "Fix N+1 query in comments","Open", DateTime.Today.AddDays(-15)),
-                new Ticket(4, "Refactor DTO mapping","Ongoing", DateTime.Today.AddDays(-3)),
-                new Ticket(5, "Update Docker compose","Ongoing", DateTime.Today.AddDays(-7)),
-                new Ticket(6, "Improve validation messages","Closed", DateTime.Today.AddDays(-5)),
-                new Ticket(7, "Add role-based authorization","Closed", DateTime.Today.AddDays(-12)),
-                new Ticket(8, "Fix timezone bug","Open", DateTime.Today.AddDays(-21)),
+                new Ticket(1, "Login fails on invalid password",TicketStatus.Open, DateTime.Today.AddDays(-1)),
+                new Ticket(2, "Add pagination to tickets endpoint",TicketStatus.Open, DateTime.Today.AddDays(-4)),
+                new Ticket(3, "Fix N+1 query in comments",TicketStatus.Open, DateTime.Today.AddDays(-15)),
+                new Ticket(4, "Refactor DTO mapping",TicketStatus.Ongoing, DateTime.Today.AddDays(-3)),
+                new Ticket(5, "Update Docker compose",TicketStatus.Ongoing, DateTime.Today.AddDays(-7)),
+                new Ticket(6, "Improve validation messages",TicketStatus.Closed, DateTime.Today.AddDays(-5)),
+                new Ticket(7, "Add role-based authorization",TicketStatus.Closed, DateTime.Today.AddDays(-12)),
+                new Ticket(8, "Fix timezone bug",TicketStatus.Open, DateTime.Today.AddDays(-21)),
             };
         }
     }
