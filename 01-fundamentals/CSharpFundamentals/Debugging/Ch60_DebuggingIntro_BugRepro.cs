13d1dbc74847415f3920c710761221a551ef2e32
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-02-23 18:43:04 +0200

    Alterations on base code

--- 01-fundamentals/CSharpFundamentals/Debugging/Ch60_DebuggingIntro_BugRepro.cs
+++ 01-fundamentals/CSharpFundamentals/Debugging/Ch60_DebuggingIntro_BugRepro.cs
@@ -1,17 +1,60 @@
 ﻿using System;
-using CSharpFundamentals.ChapterSupport;
 using System.Collections.Generic;
+using CSharpFundamentals.ChapterSupport;
 using CSharpFundamentals.Debugging;
 using System.Text;
 using System.Linq;
-
 
 namespace CSharpFundamentals.Debugging
 {
     internal class Ch60_DebuggingIntro_BugRepro
     {
         public static void Run(string[] args)
+        {
+            {
+                var numbers = new List<int> { 8, 9, 10, 11, 12, 13 };
+                var smallests = GetSmallests(numbers, 3);
 
+                foreach (var number in smallests)
+                    Console.WriteLine(number);
+
+                Console.ReadLine();
+            }
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(list);
+                smallests.Add(minim);
+                list.Remove(minim);
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is the smallest
+            var min = list[0];
+
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+}
+
+// Example code before changes
+/*namespace CSharpFundamentals.Debugging
+{
+    internal class Ch60_DebuggingIntro_BugRepro
+    {
+        public static void Run(string[] args)
         {
             {
                 var numbers = new List<int> { 8, 9, 10, 11, 12, 13 };
@@ -49,6 +92,6 @@ namespace CSharpFundamentals.Debugging
                 return min;
             }
         }
-    }
+    }*/
 
 
