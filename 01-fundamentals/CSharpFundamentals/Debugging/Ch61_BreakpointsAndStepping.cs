740c10ca9a7fe58eae06f95b22b973ccc69b5c22
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-02 15:29:14 +0200

    Replacement of problematic code

--- 01-fundamentals/CSharpFundamentals/Debugging/Ch61_BreakpointsAndStepping.cs
+++ 01-fundamentals/CSharpFundamentals/Debugging/Ch61_BreakpointsAndStepping.cs
@@ -21,9 +21,40 @@ namespace CSharpFundamentals.Debugging
     {
         public static void Run()
         {
+            {
+                var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+                var smallests = GetSmallests(numbers, 3);
 
+                foreach (var number in smallests)
+                    Console.WriteLine(number);
 
-            Console.ReadKey();
+                Console.ReadLine();
+            }
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(list);
+                smallests.Add(minim);
+                list.Remove(minim);
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is the smallest
+            var min = list[0];
+
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] > min) // bug
+                    min = list[i];
+            }
+            return min;
         }
     }
 }
