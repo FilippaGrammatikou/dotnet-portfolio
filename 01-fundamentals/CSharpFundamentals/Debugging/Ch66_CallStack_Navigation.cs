edf4c5fb755b8831b2b822250e71927c8dae0859
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-05 12:32:33 +0200

    Base Program corrected for tutorial

--- 01-fundamentals/CSharpFundamentals/Debugging/Ch66_CallStack_Navigation.cs
+++ 01-fundamentals/CSharpFundamentals/Debugging/Ch66_CallStack_Navigation.cs
@@ -18,9 +18,46 @@ namespace CSharpFundamentals.Debugging
     {
         public static void Run()
         {
+            var numbers = new List<int> { 1, 2 };
+            var smallests = GetSmallests(numbers, 3); 
+            
+            foreach (var number in smallests)
+                Console.WriteLine(number);
 
+            Console.ReadLine();
+        }
 
-            Console.ReadKey();
+        public static List<int> GetSmallests(List<int> list, int count) 
+        {
+            // if (list == null) // ArgumentNullException
+            // throw new ArgumentException("list");
+
+            if (count > list.Count || count <= 0) 
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in this list."); // Meaningful Exception Message
+     
+
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(buffer);
+                smallests.Add(minim);
+                buffer.Remove(minim);
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is the smallest
+            var min = list[0]; 
+
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
         }
     }
 }
