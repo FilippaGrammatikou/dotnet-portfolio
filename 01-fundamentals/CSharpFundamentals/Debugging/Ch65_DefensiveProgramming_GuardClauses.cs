5a35703f4b454f72bf7ec9aee4e3ede96ce210f2
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-05 12:27:18 +0200

    Ch65  Defensive Programming, ArgumentOutOfRangeException,   ArgumentNullException,   Meaningful Exception Message

--- 01-fundamentals/CSharpFundamentals/Debugging/Ch65_DefensiveProgramming_GuardClauses.cs
+++ 01-fundamentals/CSharpFundamentals/Debugging/Ch65_DefensiveProgramming_GuardClauses.cs
@@ -19,9 +19,49 @@ namespace CSharpFundamentals.Debugging
     {
         public static void Run()
         {
+            var numbers = new List<int> { 1, 2 }; 
+            var smallests = GetSmallests(numbers, 3); //client of method sends wrong data for exception. Must be between 1 and no. of list elements.
+            // var smallests = GetSmallests(null, 3); // if instead of numbers, we used null, we would get a no-reference exception, which we handle bellow
+            foreach (var number in smallests)
+                Console.WriteLine(number);
 
+            Console.ReadLine();
+        }
 
-            Console.ReadKey();
+        public static List<int> GetSmallests(List<int> list, int count) //list will cause side effects
+            // conditional statement used here to ensure Count is not greater than the number of elements in this list
+        {
+            // if (list == null) // ArgumentNullException
+            // throw new ArgumentException("list");
+
+            if (count > list.Count || count <=0 ) // check for count being larger than the no. of elements in this list
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in this list."); // Meaningful Exception Message
+            // now problem is PARTIALLY solved. Prevents application from entering the wrong state
+
+            var buffer = new List<int>(list); 
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(buffer); 
+                smallests.Add(minim);
+                buffer.Remove(minim); 
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is the smallest
+            var min = list[0]; //failure point
+            // to solve, we may either use a conditional statement here to ensure he list will not be empty, but
+            // it is wiser to use one in thr GetSmallests method instead
+
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
         }
     }
 }
