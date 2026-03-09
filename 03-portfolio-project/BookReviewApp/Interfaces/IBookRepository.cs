db483c472c0772b925b01c9986d9f13293d87c45
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-09 18:30:43 +0200

    BookRepository and IBookRepository SetUp

---
+++ 03-portfolio-project/BookReviewApp/Interfaces/IBookRepository.cs
@@ -0,0 +1,8 @@
+﻿using BookReviewApp.Models;
+namespace BookReviewApp.Interfaces
+{
+    public interface IBookRepository
+    {
+        ICollection<Book> GetAllBooks();
+    }
+}
---
+++ 03-portfolio-project/BookReviewApp/Repository/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookReviewApp.DATA;
+using BookReviewApp.Interfaces;
+using BookReviewApp.Models;
+
+namespace BookReviewApp.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly DataContext _context;
+
+        public BookRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Book> GetAllBooks()
+        {
+            return _context.Books.OrderBy(b=> b.Id).ToList();
+        }
+    }
+}
