08ac91521914f8e9c65734bd1770d2400efd74d7
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-09 12:32:41 +0200

    Database Map for BookReviewApp creation and Onmodel Creating

--- 03-portfolio-project/BookReviewApp/DATA/DataContext.cs
+++ 03-portfolio-project/BookReviewApp/DATA/DataContext.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookReviewApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookReviewApp.DATA
 {
     public class DataContext : DbContext
     {
-        public DataContext( DbContextOptions) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        //method that allows us to customize how EF builds tables and relationships
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.Category });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- 03-portfolio-project/BookReviewApp/Models/BookCategory.cs
+++ 03-portfolio-project/BookReviewApp/Models/BookCategory.cs
@@ -2,7 +2,7 @@
 {
     public class BookCategory
     {
-        public int Id { get; set; }
+        public int BookId { get; set; }
         public int CategoryId { get; set; }
         public Book Book { get; set; }
         public Category Category { get; set; }
