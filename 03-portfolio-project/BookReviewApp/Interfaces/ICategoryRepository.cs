958dacdcecc52d2dd94859d67ec889253b223dee
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-10 20:59:49 +0200

    ICategoryRepository Interface Set Up

---
+++ 03-portfolio-project/BookReviewApp/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookReviewApp.Models;
+namespace BookReviewApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int categoryId);
+        ICollection<Book> GetBooksByCategory(int categoryId);
+        bool CategoryExists(int categoryId);
+    }
+}
