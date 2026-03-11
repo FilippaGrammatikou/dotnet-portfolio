31d77b65ab2aa5b39f0b67dc13e8f5d6d32ab86d
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-11 12:49:23 +0200

    Country Repository Set Up

---
+++ 03-portfolio-project/BookReviewApp/Repository/CountryRepository.cs
@@ -0,0 +1,42 @@
+﻿using BookReviewApp.DATA;
+using BookReviewApp.Interfaces;
+using BookReviewApp.Models;
+
+namespace BookReviewApp.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly DataContext _context;
+        public CountryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.OrderBy(c => c.Id).ToList();
+        }
+
+        public Country GetCountry(int countryId)
+        {
+            return _context.Countries.Where(c=>c.Id== countryId).FirstOrDefault();
+        }
+
+        public Country GetCountryByAuthor(int authorId)
+        {
+            var author = _context.Authors.Where(a => a.Id == authorId).FirstOrDefault();
+            if (author == null)
+                return null;
+
+            return _context.Countries.Where(c=>c.Id==author.Country.Id).FirstOrDefault();
+        }
+        public bool CountryExists(int countryId)
+        {
+            return _context.Countries.Any(c => c.Id==countryId);
+        }
+
+        public ICollection<Author> GetAuthorFromCountry(int countryId)
+        {
+            return _context.Authors.Where(a=>a.Country.Id==countryId).ToList();
+        }
+    }
+}
