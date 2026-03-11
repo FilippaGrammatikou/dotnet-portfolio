4168ebf59d6ffc1daafeb8e0e18588d9cb67cbdf
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-11 12:35:34 +0200

    ICountryRepository Interface SetUp

---
+++ 03-portfolio-project/BookReviewApp/Interfaces/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookReviewApp.Models;
+
+namespace BookReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int countryId);
+        Country GetCountryByAuthor(int authorId);
+        bool CountryExists(int countryId);
+        ICollection<Author> GetAuthorsFromCountry(int countryId);
+    }
+}
