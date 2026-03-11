9c17e5692f2a6d7129dec1c8fdff2266c6941517
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-11 12:30:10 +0200

    Update Category Controller

--- 03-portfolio-project/BookReviewApp/Controllers/CategoriesController.cs
+++ 03-portfolio-project/BookReviewApp/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using BookReviewApp.Interfaces;
+﻿using BookReviewApp.DTO;
+using BookReviewApp.Interfaces;
 using BookReviewApp.Models;
 using BookReviewApp.Repository;
 using Microsoft.AspNetCore.Components;
@@ -17,27 +18,40 @@ namespace BookReviewApp.Controllers
         }
 
         [HttpGet]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<Models.Category>))]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<CategoryDto>))]
         public IActionResult GetCategories()
         {
             var categories = _categoryRepository.GetCategories();
+            var categoriesDtos = categories.Select(c => new CategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+            }).ToList();
+
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            return Ok(categories);
+            return Ok(categoriesDtos);
         }
 
         [HttpGet("{categoryId}")]
-        [ProducesResponseType(200, Type = typeof(Models.Category))]
+        [ProducesResponseType(200, Type = typeof(CategoryDto))]
         [ProducesResponseType(400)]
         public IActionResult GetCategory(int id)
         {
             if (!_categoryRepository.CategoryExists(id))
                 return BadRequest("Category not found");
+
              var category = _categoryRepository.GetCategory(id);
+            var categoryDto = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+
              if(!ModelState.IsValid)
                 return BadRequest(ModelState);
-             return Ok(category);  
+             return Ok(categoryDto);  
         }
 
         [HttpGet("{categoryId}/books")]
