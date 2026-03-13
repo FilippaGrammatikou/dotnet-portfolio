d6635f0e9ebb5c4c0cc032a7bd1ca480990bb450
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-13 15:09:43 +0200

    IReviewRepository Interface SetUp

---
+++ 03-portfolio-project/BookReviewApp/Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookReviewApp.Models;
+
+namespace BookReviewApp.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfBook(int bookId);
+        bool ReviewExists(int reviewId);
+    }
+}
