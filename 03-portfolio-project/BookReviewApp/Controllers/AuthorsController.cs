cc8ed4185a04ab814fb02639e8851990d81d6df2
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-13 15:06:19 +0200

    Author DTO And AuthorController SetUp

---
+++ 03-portfolio-project/BookReviewApp/Controllers/AuthorsController.cs
@@ -0,0 +1,97 @@
+﻿using BookReviewApp.DTO;
+using BookReviewApp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+    namespace BookReviewApp.Controllers
+        {
+            [Route("api/[controller]")]
+            [ApiController]
+            public class AuthorsController : ControllerBase
+            {
+                private readonly IAuthorRepository _authorRepository;
+                public AuthorsController(IAuthorRepository authorRepository)
+                {
+                    _authorRepository = authorRepository;
+                }
+
+                [HttpGet]
+                [ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+                public IActionResult GetAuthors()
+                {
+                    var authors = _authorRepository.GetAllAuthors().Select(a=> new AuthorDto
+                    {
+                        Id = a.Id,
+                        Name = a.Name,
+                        Bio = a.Bio,
+                    }
+                    );
+                    if (!ModelState.IsValid)
+                        return BadRequest(ModelState);
+                    return Ok(authors);
+                }
+
+                [HttpGet("{authorId}")]
+                [ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+                [ProducesResponseType(400)]
+                public IActionResult GetAuthor(int authorId)
+                {
+                    if (!_authorRepository.AuthorExists(authorId))
+                        return BadRequest("Author not found.");
+
+                    var author = _authorRepository.GetAuthorById(authorId);
+                    var authorDto = new AuthorDto
+                    {
+                        Id = author.Id,
+                        Name = author.Name,
+                        Bio = author.Bio,
+                    };
+
+                    if (!ModelState.IsValid)
+                        return BadRequest(ModelState);
+                    return Ok(author);
+                }
+
+                [HttpGet("book/{bookId}")]
+                [ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+                [ProducesResponseType(400)]
+                public IActionResult GetAuthorOfABook(int bookId)
+                {
+                    var author = _authorRepository.GetAuthorsOfBook(bookId).Select(a => new AuthorDto
+                    {
+                        Id = a.Id,
+                        Name = a.Name,
+                        Bio = a.Bio,
+                    });
+
+                    if (author == null)
+                        return NotFound("Book not found or has no author assigned");
+
+                    if (!ModelState.IsValid)
+                        return BadRequest(ModelState);
+
+                    return Ok(author);
+                }
+
+                [HttpGet("{authorId}/books")]
+                [ProducesResponseType(200, Type = typeof(IEnumerable<BookDto>))]
+                [ProducesResponseType(400)]
+                public IActionResult GetBooksByAuthor(int authorId)
+                {
+                    if (!_authorRepository.AuthorExists(authorId))
+                        return BadRequest("Author not found.");
+
+                    var books = _authorRepository.GetBooksByAuthor(authorId).Select(a=>new BookDto
+                    {
+                        Id = a.Id,
+                        Title = a.Title,
+                        ReleaseDate = a.ReleaseDate,
+
+                    });
+
+                    if (!ModelState.IsValid)
+                        return BadRequest(ModelState);
+                    return Ok(books);
+                }
+            }
+        }
---
+++ 03-portfolio-project/BookReviewApp/DTO/AuthorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookReviewApp.DTO
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+    }
+}
